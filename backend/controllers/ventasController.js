3e49d2d053b2e263bde668fa61297e5a5d770531
Brad312-oss <brolymaster312@gmail.com>
2025-05-22 13:54:52 -0500

    Tercer Commit, Controlador

---
+++ backend/controllers/ventasController.js
@@ -0,0 +1,24 @@
+import {
+    obtenerVentas as obtenerVentasModel,
+    registarVenta as registarVentaModel
+}
+    from '../models/ventaModel.js';
+
+    export const registarVenta = (req, res) => {
+        const venta = req.body;
+
+        if (!venta.id_cliente || !venta.id_producto || !venta.cantidad){
+            return res.status(400).json({error: 'Faltan campos requeridos'});
+        }
+        registarVentaModel(venta, (err, resultado) => {
+            if (err) return res.status(500).json({ error: err.message});
+            res.status(201).json({ mensaje: 'Venta Registrada Exitosamente', id:resultado.insertId});
+        });
+    };
+
+export const obtenerVentas = (req, res) => {
+    obtenerVentasModel((err, ventas) => {
+        if (err) return res.status(500).json({ error: err.message});
+        res.json(ventas);
+    })
+}
